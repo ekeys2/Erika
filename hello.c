6f04a6b76bfa0aab9be22b11b34641cbf26be55a
ekeys2 <42587925+ekeys2@users.noreply.github.com>
2018-08-23 14:32:34 -0500

    Hack 1.0

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Erika Keys
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Erika-Keys\n");
+
+  return 0;
+}
